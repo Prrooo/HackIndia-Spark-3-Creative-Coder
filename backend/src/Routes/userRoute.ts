53e847e32760d3b92a86e2928fada56d1fcd1e55
Prrooo <harshmalik00011@gmail.com>
2024-08-28 03:31:04 +0530

    package src changes

--- backend/src/Routes/userRoute.ts
+++ backend/src/Routes/userRoute.ts
@@ -11,11 +11,6 @@ const router = express.Router();
 
 
 router.post('/signin', siginController);
-router.post('/presignedUrl', authMiddleware, (req, res) => {
-    return res.status(200).json({
-        message: "working"
-    })
-});
 router.post('/task', authMiddleware, taskController)
 router.get("/task/:id", authMiddleware, getAllTask);
 router.get("/presignedUrl", authMiddleware,imageURLImport)
